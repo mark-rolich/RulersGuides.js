af8be7277c58380d4322df96d077fa81f5b5c1e8
mark.rolich <mark.rolich@tumo.org>
2013-09-19 12:38:39 +0400

    Added info box to guides displaying current position, minor improvements

--- RulersGuides.js
+++ RulersGuides.js
@@ -90,7 +90,6 @@ var RulersGuides = function (evt, dragdrop) {
         },
         hRuler      = null,
         vRuler      = null,
-        guide       = null,
         mode        = 2,
         guides      = {},
         guidesCnt   = 0,
@@ -145,7 +144,7 @@ var RulersGuides = function (evt, dragdrop) {
                 hRuler.style.display = 'none';
             }
         },
-        cssText     = 'html,body{margin:0;padding:0}.guide{position:absolute;top:0;left:0;z-index:9999;font-size:0}.guide.v{width:1px;border-right:solid 1px #00f;cursor:col-resize}.guide.h{height:1px;border-bottom:solid 1px #00f;cursor:row-resize}.ruler{-moz-user-select:-moz-none;-khtml-user-select:none;-webkit-user-select:none;-ms-user-select:none;user-select:none;background-color:#ccc;position:absolute;top:0;left:0;z-index:9998}.ruler .label{font:12px Arial;color:#000}.ruler,.ruler span{font-size:0}.ruler.h{left:-1px;padding-top:14px;border-bottom:solid 1px #000}.ruler.v{top:-1px;padding-left:16px;width:25px;border-right:solid 1px #000}.ruler.h span{border-left:solid 1px #999;height:9px;width:1px;vertical-align:bottom;display:inline-block;*display:inline;zoom:1}.ruler.v span{display:block;margin-left:auto;margin-right:0;border-top:solid 1px #999;width:9px;height:1px}.ruler.v span.major{border-top:solid 1px #000;width:13px}.ruler.v span.milestone{position:relative;border-top:solid 1px #000;width:17px}.ruler.v span.label{border:0;font-size:9px;position:absolute;text-align:center;width:9px}.ruler.h span.major{border-left:solid 1px #000;height:13px}.ruler.h span.milestone{position:relative;border-left:solid 1px #000;height:17px}.ruler.h span.label{border:0;font-size:9px;position:absolute;text-align:center;top:-14px;width:9px}.ruler.h .l10{left:-5px}.ruler.h .l100{left:-7px}.ruler.h .l1000{left:-10px}.ruler.v .l10,.ruler.v .l100,.ruler.v .l1000{top:-7px}.ruler.v .l10{left:-12px}.ruler.v .l100{left:-17px}.ruler.v .l1000{left:-23px}',
+        cssText     = 'html,body{margin:0;padding:0}.guide{position:absolute;top:0;left:0;z-index:9999;font-size:0}.guide.v{width:1px;border-right:solid 1px #00f;cursor:col-resize}.guide.h{height:1px;border-bottom:solid 1px #00f;cursor:row-resize}.info{width:50px;height:25px;line-height:25px;text-align:center;position:relative;font-size:13px;background-color:#eee;border:solid 1px #ccc;color:#000}.guide.v .info{left:2px}.guide.h .info{top:2px}.ruler{-moz-user-select:-moz-none;-khtml-user-select:none;-webkit-user-select:none;-ms-user-select:none;user-select:none;background-color:#ccc;position:absolute;top:0;left:0;z-index:9998}.ruler .label{font:12px Arial;color:#000}.ruler,.ruler span{font-size:0}.ruler.h{left:-1px;padding-top:14px;border-bottom:solid 1px #000}.ruler.v{top:-1px;padding-left:16px;width:25px;border-right:solid 1px #000}.ruler.h span{border-left:solid 1px #999;height:9px;width:1px;vertical-align:bottom;display:inline-block;*display:inline;zoom:1}.ruler.v span{display:block;margin-left:auto;margin-right:0;border-top:solid 1px #999;width:9px;height:1px}.ruler.v span.major{border-top:solid 1px #000;width:13px}.ruler.v span.milestone{position:relative;border-top:solid 1px #000;width:17px}.ruler.v span.label{border:0;font-size:9px;position:absolute;text-align:center;width:9px}.ruler.h span.major{border-left:solid 1px #000;height:13px}.ruler.h span.milestone{position:relative;border-left:solid 1px #000;height:17px}.ruler.h span.label{border:0;font-size:9px;position:absolute;text-align:center;top:-14px;width:9px}.ruler.h .l10{left:-5px}.ruler.h .l100{left:-7px}.ruler.h .l1000{left:-10px}.ruler.v .l10,.ruler.v .l100,.ruler.v .l1000{top:-7px}.ruler.v .l10{left:-12px}.ruler.v .l100{left:-17px}.ruler.v .l1000{left:-23px}',
         prepare     = function () {
             var style = document.createElement('style');
 
@@ -201,29 +200,79 @@ var RulersGuides = function (evt, dragdrop) {
                 (e.clientY > hBound && e.clientX < vBound)
             ) && rulerStatus === 1
         ) {
-            guide = document.createElement('div');
+            var guide = document.createElement('div'),
+                guideInfo = guide.cloneNode(false),
+                guideInfoText = document.createTextNode('');
+
             gUid = 'guide-' + guidesCnt;
+
+            guideInfo.className = 'info';
+
+            guideInfo.appendChild(guideInfoText);
+            guide.appendChild(guideInfo);
 
             if (e.clientX > vBound && e.clientY < hBound) {
                 guide.className = 'guide h draggable';
                 guide.style.top = e.clientY + 'px';
                 guide.style.width = gWidth + 'px';
+                guideInfo.style.left = (e.clientX + 10) + 'px';
+
                 mode = 2;
             } else if (e.clientY > hBound && e.clientX < vBound) {
                 guide.className = 'guide v draggable';
                 guide.style.left = e.clientX + 'px';
                 guide.style.height = gHeight + 'px';
+                guideInfo.style.top = (e.clientY - 35) + 'px';
+
                 mode = 1;
             }
 
             guide.id = gUid;
+            guide.info = guideInfo;
+            guide.text = guideInfoText;
+
             guides[gUid] = guide;
 
             document.body.appendChild(guide);
 
-            guide = document.getElementById(gUid);
+            dragdrop.set(guide, {
+                mode: mode,
+                onstart: function (elem) {
+                    var text = (elem.mode === 1)
+                            ? parseInt(elem.style.left, 10) + 2
+                            : parseInt(elem.style.top, 10) + 2;
 
-            dragdrop.set(guide, {mode: mode});
+                    elem.text.nodeValue = text;
+
+                    if (elem.over !== undefined) {
+                        evt.detach('mouseover', elem, elem.over);
+                        evt.detach('mouseout', elem, elem.out);
+                    }
+                },
+                onmove: function (elem) {
+                    var text = (elem.mode === 1)
+                            ? parseInt(elem.style.left, 10) + 2
+                            : parseInt(elem.style.top, 10) + 2;
+
+                    elem.text.nodeValue = text;
+                },
+                onstop: function (elem) {
+                    elem.over = evt.attach('mouseover', elem, function (e, src) {
+                        if (src.className === 'guide v draggable') {
+                            elem.info.style.top = (e.clientY - 35) + 'px';
+                        } else if (src.className === 'guide h draggable') {
+                            elem.info.style.left = (e.clientX + 10) + 'px';
+                        }
+
+                        elem.info.style.display = 'block';
+                    });
+
+                    elem.out = evt.attach('mouseout', elem, function () {
+                        elem.info.style.display = 'none';
+                    });
+                }
+            });
+
             dragdrop.start(e, guide);
 
             guidesCnt = guidesCnt + 1;
